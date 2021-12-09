ae3cbfc01b9b18f16ee59787ae17a07fdce8daa2
Imran Razak <Imranrazak1@icloud.com>
2021-12-09 11:39:18 +0000

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Imran razak on 09/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
